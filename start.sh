a1b2b0b378f8b1bc4cdd321ba342c47cfbfd034e
yzqiang666 <yzqiang6@gmail.com>
2021-09-03 18:49:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ rm -rf wwwroot.tar.gz
 echo ============ PORT=${PORT} ====================
 sed -i -E "s/http.d/conf.d/" /etc/nginx/nginx.conf
 sed -i -E "s/TO_BE_REPLACED_WITH_PORT/${PORT:-8888}/" /etc/nginx/conf.d/*.conf
-sed -i -E "s/80/${PORT:-8888}/" /etc/nginx/http.d/*.conf
+###sed -i -E "s/80/${PORT:-8888}/" /etc/nginx/http.d/*.conf
 # "/var/tmp/nginx" owned by "nginx" user is unusable on heroku dyno so re-create on runtime
 mkdir /var/tmp/nginx
 
